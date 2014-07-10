52439f845869f422436ddabfadb2d7b969c79833
wangyuting <wytthuss@126.com>
2014-07-10 14:50:23 +0800

    no message

---
+++ Homework4/jquery/myjquery.js
@@ -0,0 +1,53 @@
+$(".right").click(function(){
+	id = (Number(id)+1)%5;
+	b[id].click();
+	}
+);
+$(".left").click(function(){
+	id = (Number(id)-1+5)%5;
+	b[id].click();
+}
+);
+
+
+var b = $(".but");
+var id = 0;
+var pre = 0;
+f = $(".pic")[pre];
+$(f).css("display", "block");
+f = $(".show")[pre];
+$(f).css("display", "block");
+
+for(var i = 0; i < b.length; i++){
+	$(b[i]).click(function(){
+		id = $(this).attr("id");
+		if(pre != id){
+			var pic = $("li.pic");	
+			var sho = $("li.show");
+			$(pic[pre]).fadeTo(0.5, 70);
+			$(pic[id]).fadeIn(800);
+			$(sho[id]).fadeIn(10);
+			$(pic[pre]).fadeOut(10);
+			$(sho[pre]).fadeOut(10);
+			pre = id;
+			}
+		}
+	);
+}
+setInterval("roll()", 2000);
+function roll()
+{
+	$(".right").click();
+}
+
+
+
+
+
+
+
+
+
+
+
+
