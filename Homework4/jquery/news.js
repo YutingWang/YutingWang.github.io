95bc2b69195f6b59687c1e976aa28d5440442c09
wangyuting <wytthuss@126.com>
2014-07-10 14:18:05 +0800

    no message

--- Homework4/jquery/news.js
+++ Homework4/jquery/news.js
@@ -2,22 +2,22 @@
     images: [
         {
             url:'https://YutingWang.github.io/Homework4/pic/1.jpg',
-            title:'吾王归来!Fate新作官网公开',
+            title:'吾王归来!Fate新作官网公开'
         },
         {
             url:'https://YutingWang.github.io/Homework4/pic/2.jpg',
-            title:'X战警：逆转未来',
+            title:'X战警：逆转未来'
         },
         {
             url:'https://YutingWang.github.io/Homework4/pic/3.jpg',
-            title:'FROZEN：姐嫁奇缘',
+            title:'FROZEN：姐嫁奇缘'
         },
         {
             url:'https://YutingWang.github.io/Homework4/pic/4.jpg',
-            title:'吾王归来!Fate新作官网公开',
+            title:'吾王归来!Fate新作官网公开'
         },
         {
             url:'https://YutingWang.github.io/Homework4/pic/5.jpg',
-            title:'X战警：逆转未来',
+            title:'X战警：逆转未来'
         }]
 }
