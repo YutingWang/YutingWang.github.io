f6859e348c3356357ac135728c17d56e2094a967
wangyuting <wytthuss@126.com>
2014-07-10 13:55:39 +0800

    hw4

---
+++ Homework4/jquery/news.js
@@ -0,0 +1,16 @@
+{
+    images: [
+        {
+            url:'https://wanganqi.github.io/hw_4/picture/fateStayNight.JPG',
+            title:'吾王归来!Fate新作官网公开',
+        },
+        {
+            url:'https://wanganqi.github.io/hw_4/picture/x4.jpeg',
+            title:'X战警：逆转未来',
+        },
+        {
+            url:'https://wanganqi.github.io/hw_4/picture/frozen.jpeg',
+            title:'FROZEN：姐嫁奇缘',
+        }
+    ]
+}
