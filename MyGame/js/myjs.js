37a73f35b50daa3660a85ffdc200312061c7d19c
wangyuting <wytthuss@126.com>
2014-07-13 15:44:54 +0800

    no message

--- MyGame/js/myjs.js
+++ MyGame/js/myjs.js
@@ -1,11 +1,13 @@
 //=================本地存储==========================
 var storage = window.localStorage;
 if(storage.level == undefined) storage.level = 1;
+loadLevel();
+loadUrl();
 
-
-//=================载入关卡中元素位置===========================
-$(document).ready(function(){
-		    $(".toDelete").load('js/leve'+storage.level+'.json',function(responseTxt,statusTxt,xhr){
+//=================载入关卡中元素位置===========levelObj================
+function loadLevel(){
+	$("toDelete").ready(function(){
+		    $(".toDelete").load('https://YutingWang.github.io/MyGame/js/level'+storage.level+'.json',function(responseTxt,statusTxt,xhr){
 		    	if(statusTxt=="success")
 		      	{
 		       		data = responseTxt;
@@ -14,4 +16,19 @@ $(document).ready(function(){
 		      	else if(statusTxt=="error") alert("Error: "+xhr.status+": "+xhr.statusText);
 		      	$(".toDelete").css("display", "none");	
 		  	});
-		});
+	});
+}
+//=================载入关卡中icon图标地址======urlObj===================
+function loadUrl(){
+	$("toDelete").ready(function(){
+		    $(".toDelete").load('https://YutingWang.github.io/MyGame/js/icon.json',function(responseTxt,statusTxt,xhr){
+		    	if(statusTxt=="success")
+		      	{
+		       		data = responseTxt;
+		      		urlObj = eval('('+responseTxt+')');
+		      	}
+		      	else if(statusTxt=="error") alert("Error: "+xhr.status+": "+xhr.statusText);
+		      	$(".toDelete").css("display", "none");	
+		  	});
+	});
+}
