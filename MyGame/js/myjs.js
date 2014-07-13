9677b120ca31bfd5fc39d73cc7d741476b7137b7
wangyuting <wytthuss@126.com>
2014-07-14 00:50:06 +0800

    no message

--- MyGame/js/myjs.js
+++ MyGame/js/myjs.js
@@ -32,3 +32,26 @@ function loadUrl(){
 		  	});
 	});
 }
+
+//===========载入激光器============================
+for(var i = 0; i < levelObj.laser.length; i++)
+{
+	var color = levelObj.laser[i].color;
+	var x = levelObj.laser[i].x;
+	var y = levelObj.laser[i].y;
+	var box = $("td")[Number(y*12)+Number(x)];
+	var url = urlObj.url.laser[c];
+	var ddd = "url"+"("+"'"+url+"'"+")";
+	$(box).css("background-image", ddd);
+}
+//===========载入target============================
+for(var i = 0; i < levelObj.target.length; i++)
+{
+	var color = levelObj.target[i].color;
+	var x = levelObj.target[i].x;
+	var y = levelObj.target[i].y;
+	var box = $("td")[Number(y*12)+Number(x)];
+	var url = urlObj.url.target[c+'1'];
+	var ddd = "url"+"("+"'"+url+"'"+")";
+	$(box).css("background-image", ddd);
+}
