fe3ca7473940ebc8c7c55bb59330dbdbacc2c9e6
wangyuting <wytthuss@126.com>
2014-07-14 00:59:08 +0800

    no message

--- MyGame/js/myjs.js
+++ MyGame/js/myjs.js
@@ -3,6 +3,7 @@ var storage = window.localStorage;
 if(storage.level == undefined) storage.level = 1;
 loadLevel();
 loadUrl();
+place();
 
 //=================载入关卡中元素位置===========levelObj================
 function loadLevel(){
@@ -33,25 +34,29 @@ function loadUrl(){
 	});
 }
 
-//===========载入激光器============================
-for(var i = 0; i < levelObj.laser.length; i++)
-{
-	var color = levelObj.laser[i].color;
-	var x = levelObj.laser[i].x;
-	var y = levelObj.laser[i].y;
-	var box = $("td")[Number(y*12)+Number(x)];
-	var url = urlObj.url.laser[c];
-	var ddd = "url"+"("+"'"+url+"'"+")";
-	$(box).css("background-image", ddd);
-}
-//===========载入target============================
-for(var i = 0; i < levelObj.target.length; i++)
-{
-	var color = levelObj.target[i].color;
-	var x = levelObj.target[i].x;
-	var y = levelObj.target[i].y;
-	var box = $("td")[Number(y*12)+Number(x)];
-	var url = urlObj.url.target[c+'1'];
-	var ddd = "url"+"("+"'"+url+"'"+")";
-	$(box).css("background-image", ddd);
+function place(){
+	$("toDelete").ready(function(){
+		//===========载入激光器============================
+		for(var i = 0; i < levelObj.laser.length; i++)
+		{
+			var color = levelObj.laser[i].color;
+			var x = levelObj.laser[i].x;
+			var y = levelObj.laser[i].y;
+			var box = $("td")[Number(y*12)+Number(x)];
+			var url = urlObj.url.laser[c];
+			var ddd = "url"+"("+"'"+url+"'"+")";
+			$(box).css("background-image", ddd);
+		}
+		//===========载入target============================
+		for(var i = 0; i < levelObj.target.length; i++)
+		{
+			var color = levelObj.target[i].color;
+			var x = levelObj.target[i].x;
+			var y = levelObj.target[i].y;
+			var box = $("td")[Number(y*12)+Number(x)];
+			var url = urlObj.url.target[c+'1'];
+			var ddd = "url"+"("+"'"+url+"'"+")";
+			$(box).css("background-image", ddd);
+		}
+	});
 }
