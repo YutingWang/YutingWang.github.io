09f7bd21520862e5765632c70fe3c317c0c7099d
wangyuting <wytthuss@126.com>
2014-07-15 11:25:10 +0800

    no message

--- MyGame/js/myjs_level.js
+++ MyGame/js/myjs_level.js
@@ -109,18 +109,16 @@ function loadMirror(){
 		$("td").attr("ondrop", "drop(event)");
 		$("td").attr("ondragover", "allowDrop(event)");
 		for(var i = 0; i < levelObj.mirror.length; i++)
-			a += Number(levelObj.mirror[i].num);
-		for(var i = 0; i < a; )
 		{
 			var mid = levelObj.mirror[i].mtype;
 			if(mid == '90') flag = 2;
 			else if(mid == "45") flag = 3;
 			else if(mid == "0") flag = 4;
-			for(var j = 0; j < levelObj.mirror[i].num; i++,j++)
+			for(var j = 0; j < levelObj.mirror[i].num; j++)
 			{
-				var myimage = '<img draggable="true" ondragstart="drag(event)" id="'+i+'"width="100%"/>';
-				$($("td")[i]).append(myimage);//TO EDIT
-				c = $($($("td")[i]).children());
+				var myimage = '<img draggable="true" ondragstart="drag(event)" id="'+i+j+'"width="100%"/>';
+				$($("td")[Number(i)+Number(j)]).append(myimage);//TO EDIT
+				c = $($($("td")[Number(i)+Number(j)]).children());
 				c.attr("src", urlObj.url.mirror["reflex"+mid]);
 				c.attr("position","0");//To EDIT
 			}			
